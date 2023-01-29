c77f105f5d6f264a9b8d92a7909df7d6b44da671
Bogdan Jianu <bogdan@questionableservices.com>
2023-01-29 22:58:57 +0200

    Created syntax highlighter component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,25 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { SyntaxHighlighter } from "./components/syntax-highlighter";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.tsx</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+  const codeBlock = `// React Component
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello {this.props.name}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <HelloMessage name="Taylor" />,
+  mountNode
+);`
+
+  return(
+      <>
+        <SyntaxHighlighter language="jsx" showLineNumbers={true} codeBlock={codeBlock}></SyntaxHighlighter>
+      </>
+  )
 }
 
 export default App;
---
+++ app/src/components/syntax-highlighter.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {CodeBlock, SupportedLanguages} from '@atlaskit/code';
+
+type Props = {
+    language: SupportedLanguages | undefined;
+    showLineNumbers: boolean;
+    codeBlock: string;
+};
+
+export const SyntaxHighlighter = ({language, showLineNumbers, codeBlock}: Props) => {
+    return (
+        <CodeBlock language={language} showLineNumbers={showLineNumbers} text={codeBlock} />
+    );
+};
